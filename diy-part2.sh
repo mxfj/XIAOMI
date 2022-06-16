8acbf04519722a2a0e541fd38d6172686de07577
HongbinZheng <myspoto@gmail.com>
2022-06-16 10:08:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 添加主题文件
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
